befcd5318f6f06726430929cd7004c15a23f25b9
Cuneyd Kaya <cgultekink@gmail.com>
2025-10-02 10:26:54 +0300

    TestRunner class was added to the project

--- src/test/java/com/sauce/runner/TestRunner.java
+++ src/test/java/com/sauce/runner/TestRunner.java
@@ -1,4 +1,19 @@
 package com.sauce.runner;
 
-public class TestRunner {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"com.sauce.steps", "com.sauce.hooks"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/cucumber.html",
+                "json:target/cucumber-reports/cucumber.json",
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+        },
+        monochrome = true,
+        dryRun = false
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
 }
