5255cbef83e5bd842d3243dc93b776770078073e
Cuneyd Kaya <cgultekink@gmail.com>
2025-10-02 10:15:40 +0300

    api client and product model were added to the project

--- src/main/java/com/sauce/api/ApiClient.java
+++ src/main/java/com/sauce/api/ApiClient.java
@@ -1,4 +1,25 @@
 package com.sauce.api;
 
+import com.sauce.api.models.Product;
+import com.sauce.config.Config;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import static org.hamcrest.Matchers.equalTo;
+
 public class ApiClient {
+    public Product getProductById(int id) {
+        return RestAssured
+                .given()
+                .baseUri(Config.apiBase())
+                .basePath("/products/" + id)
+                .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
+                .accept(ContentType.JSON)
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(id))
+                .extract()
+                .as(Product.class);
+    }
 }
--- src/main/java/com/sauce/api/models/Product.java
+++ src/main/java/com/sauce/api/models/Product.java
@@ -1,4 +1,11 @@
 package com.sauce.api.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Product {
+    public int id;
+    public String title;
+    public double price;
+
 }
